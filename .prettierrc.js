671351223dafdc2fac216959c095a2c696e6d192
Steven Truong <steven.truong@century.tech>
2023-12-01 11:51:46 +0000

    bootstrap

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  printWidth: 120,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  useTabs: false,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+}
